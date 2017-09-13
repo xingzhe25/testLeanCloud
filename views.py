50d5e4ee7f3c76a1daf76e52f7b1a8eb2cdd0688
xingzhe25 <xingzhe25@126.com>
2017-09-13 15:34:39 +0800

    0.4.1 固定内容

--- views.py
+++ views.py
@@ -123,7 +123,7 @@ class hiWechat(View):
             # 当前会话内容
             #content = message.content.strip()
             content = getwangyi("http://news.163.com/rank/")
-            response = wechat_instance.response_text(content = content)
+            response = wechat_instance.response_text(content = 'content')
 
         return HttpResponse(response, content_type="application/xml")
 
