89b2922f5d06574b8e5b6e18cfc7fdf5962b60ee
xingzhe25 <xingzhe25@126.com>
2017-09-20 13:36:57 +0800

    0.5.2 utf-8

--- views.py
+++ views.py
@@ -121,10 +121,13 @@ class hiWechat(View):
 
         if isinstance(message, TextMessage):
             # 当前会话内容
-            content = message.content.strip()
+            content = message.content.strip().decode('utf-8')
             print "**content: " + content
             #content = getwangyi("http://news.163.com/rank/")
             response = wechat_instance.response_text(content=content)
+
+            wangyiLink = getwangyi("http://news.163.com/rank/")
+            print "**wangyiLink: " + wangyiLink
 
         return HttpResponse(response, content_type="application/xml")
 
